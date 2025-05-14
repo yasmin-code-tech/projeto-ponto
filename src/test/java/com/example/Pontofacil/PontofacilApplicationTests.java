e044f49f0fb24145fd9d94224ec3020cff34cb3e
yasmin-code-tech <yasmin.jobs33@gmail.com>
2025-05-14 18:20:35 -0300

    Add files via upload

---
+++ src/test/java/com/example/Pontofacil/PontofacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pontofacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontofacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
